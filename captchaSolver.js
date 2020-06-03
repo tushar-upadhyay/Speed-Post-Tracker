ad6dc4ef7193c83408cda6c3db51ab5372d941f2
tushar-upadhyay <tusharrockpg@gmail.com>
2020-06-03 19:20:58 +0530

    fixed captcha issue

--- captchaSolver.js
+++ captchaSolver.js
@@ -8,24 +8,18 @@ let dict = {
 }
 function solve(captcha, context) {
   if (context == "Evaluate the Expression") {
-    captcha = String(captcha);
-    let data = [];
-    let counter = 0;
-    for (let char of captcha) {
-      if (counter % 2 == 0) {
-        data.push(char);
-      }
-      counter = counter + 1;
-    }
-    let a = parseInt(data[0]);
-    let b = parseInt(data[2]);
-    let operator = data[1];
+    console.log(captcha);
+    captcha = String(captcha).split(' ');
+    let a = parseInt(captcha[0]);
+    let b = parseInt(captcha[2]);
+    let operator = captcha[1];
     let result;
     if (operator == "-") {
       result = a - b;
     } else {
       result = a + b;
     }
+    console.log(result);
     return result;
   }
   else if(context=="Enter characters as displayed in image"){
--- scrapper.js
+++ scrapper.js
@@ -1,30 +1,33 @@
 var cheerio = require('cheerio');
 var axios = require('axios');
 var getFormData = require('./getParams');
-var fs = require('fs');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 async function scrap(id){
-let res = await axios.get('https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx')
-   var $ = cheerio.load(res.data);
-   let data = getFormData($,id);
-   let headers = data[1];
-   let form = data[0];
-   
-   result = await axios({
-       url:'https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx',
-       method:'POST',
-       headers:headers,
-       data:form
-   })
-   $ = cheerio.load(result.data);
-   let head1 = $('#ctl00_PlaceHolderMain_ucNewLegacyControl_divTrckMailArticleDtlsOER').html();
-   if(head1==undefined){
-       return "error"
-   }
-   let head2 = "<table id=\"t\">"
-   head2 +=$('#ctl00_PlaceHolderMain_ucNewLegacyControl_gvTrckMailArticleEvntOER').html();
-   head2+='</table>'
-   return head1+'<br>'+head2
+    try{
+        let res = await axios.get('https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx')
+        var $ = cheerio.load(res.data);
+        let data = getFormData($,id);
+        let headers = data[1];
+        let form = data[0];
+        result = await axios({
+        url:'https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx',
+        method:'POST',
+        headers:headers,
+        data:form
+        })
+        $ = cheerio.load(result.data);
+        let head1 = $('#ctl00_PlaceHolderMain_ucNewLegacyControl_divTrckMailArticleDtlsOER').html();
+        if(head1==undefined){
+            return "error"
+        }
+        let head2 = "<table id=\"t\">"
+        head2 +=$('#ctl00_PlaceHolderMain_ucNewLegacyControl_gvTrckMailArticleEvntOER').html();
+        head2+='</table>'
+        return head1+'<br>'+head2
+    }
+    catch(err){
+        return "error";
+    }
 }
 module.exports = scrap;
 
