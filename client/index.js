e4f7ad2ea8b56b9bdc966fc00ef383c7a10cab2d
tushar-upadhyay <tusharrockpg@gmail.com>
2020-06-07 22:12:46 +0530

    updated index.js

--- client/index.js
+++ client/index.js
@@ -1,25 +1,25 @@
+let result = document.getElementById('result');
 function handleSubmit(e){
     e.preventDefault();
     document.getElementById('btn').setAttribute('disabled','true');
-    document.getElementById('result').innerHTML = "";
-    document.getElementById('result').className = "loader";
+    result.innerHTML = "";
+    result.className = "loader";
     let id = e.target[0].value;
     fetch(`/search?id=${id}`)
     .then(res=>res.text())
     .then(res=>{
         document.getElementById('btn').removeAttribute('disabled');
+        result.className = "";
         if(res=='error'){
-        document.getElementById('result').className = "";
-        document.getElementById('result').innerHTML = '<br><h3>Your ID is incorrect or some server issue</h3>' 
+        result.innerHTML = '<br><h3>Your ID is incorrect or some server issue</h3>' 
         }
         else{
-        document.getElementById('result').className = "";
-        document.getElementById('result').innerHTML = res;
+        result.innerHTML = res;
         }
     })
     .catch(err=>{
-        document.getElementById('result').className = "";
-        document.getElementById('result').innerHTML = '<br><h3>Your ID is incorrect or some server issue</h3>';
+        result.className = "";
+        result.innerHTML = '<br><h3>Your ID is incorrect or some server issue</h3>';
         document.getElementById('btn').removeAttribute('disabled')
     });
     
