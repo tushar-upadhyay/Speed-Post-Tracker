915cbca1c06595ff6f1301f629418b81d47680ab
Tushar Upadhyay <47355104+tushar-upadhyay@users.noreply.github.com>
2020-09-07 11:49:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var scarp = require('./scrapper');
-var temp = require('./client/temp')
 var app =express();
 app.get(`/search`,async(req,res)=>{
     id = req.query.id==null?req.params.id:req.query.id;
-    if(id=='EN01031169IN'){
-        return res.send(temp[0])
-    }
-    if(id=='EN06316991IN'){
-        return res.send(temp[1])
-    }
     response = await scarp(id);
     res.send(response);
 })
